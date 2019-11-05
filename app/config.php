fdfebb8a3e31866f3f3444bfa004ed5fe7225d52
Xibalsba <jl573160@gmail.com>
2019-11-04 18:40:54 -0600

    Variables de configuracion para la base de datos del Framework

---
+++ app/config.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Este archivo contiene las configuraciones generales del Framework
+ */
+
+
+ /**
+  * Nombre de tu host
+  * @var string
+  */
+ define ("DB_HOST","");
+ /**
+  * Nombre de tu base de datos
+  * @var string
+  */
+ define ("DB_NAME","");
+ /**
+  * Usuario de la base de datos
+  * @var string
+  */
+ define ("DB_USER","");
+ /**
+  * Contraseña de la base de datos
+  * @var string
+  */
+ define ("DB_PASS","");
+?>
